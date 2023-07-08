c73f0cd80d06ce6099e6b021e27d0eacd51e7afb
Shumon Khan <backlink.shumonkhan@gmail.com>
2023-07-08 19:27:32 +0600

    max and min updated

---
+++ script.js
@@ -0,0 +1,23 @@
+//copy menu for mobile
+
+function copyMenu ( ) {
+
+    //copy inside desk dpt to mobile dpt
+    let dptCategory = document.querySelector('.dpt-cart');
+    let dptPlace = document.querySelector('.departments');
+    dptPlace.innerHTML = dptCategory.innerHTML;
+
+     //copy inside desk nav to mobile nav
+     let mainNav = document.querySelector('.header-nav nav');
+     let navPlace = document.querySelector('.off-canvas nav');
+     navPlace.innerHTML = mainNav.innerHTML;
+     
+     //copy inside desk topnav to mobile topnav
+     let topNav = document.querySelector('.header-top');
+     let topPlace = document.querySelector('.off-canvas .thetop-nav');
+     topPlace.innerHTML = topNav.innerHTML;
+
+
+}
+
+copyMenu()
